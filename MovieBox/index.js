788b4d9abb0eb24223a7f8eb0ee7b61e90b21dab
maysam <maysam82.dev@gmail.com>
2020-08-11 01:11:43 +1000

    Created movie search dropdown

--- MovieBox/index.js
+++ MovieBox/index.js
@@ -1,1 +1,43 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: getApiKey(),
+      s: searchTerm,
+    },
+  });
+
+  if (response.data.Error) return [];
+
+  return response.data.Search;
+};
+
+const root = document.querySelector(".authcomplete");
+root.innerHTML = `
+<label><b>Search For a Movie</b></label>
+<input class="input" />
+<div class="dropdown">
+  <div class="dropdown-menu">
+	<div class="dropdown-content results"></div>
+  </div>
+</div>
+`;
+
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+  dropdown.classList.add("is-active");
+  for (const movie of movies) {
+    const { Title, Poster } = movie;
+    const option = document.createElement("a");
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+		  <img src="${Poster === 'N/A' ? '' : Poster}" alt="${Title}"/>
+		  ${Title}
+		  `;
+    resultsWrapper.appendChild(option);
+  }
+};
+input.addEventListener("input", debounce(onInput, 500));
