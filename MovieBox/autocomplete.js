cc9953f81b26e0105c946f6a623c7464ec3826dd
maysam <maysam82.dev@gmail.com>
2020-08-11 21:42:09 +1000

    Made AutoComplete root reusable

---
+++ MovieBox/autocomplete.js
@@ -0,0 +1,53 @@
+// This function will take a configuration object to create autocomplete acc. to this congif.
+const createAutoComplete = ({ root }) => {
+  console.log(root);
+  root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+    `;
+
+  const input = root.querySelector("input");
+  const dropdown = root.querySelector(".dropdown");
+  const resultsWrapper = root.querySelector(".results");
+
+  const onInput = async (event) => {
+    const movies = await fetchData(event.target.value);
+
+    if (!movies.length) {
+      dropdown.classList.remove("is-active");
+      return;
+    }
+
+    //  make `resultWrapper` empty if there is a previous fetch.
+    resultsWrapper.innerHTML = "";
+    dropdown.classList.add("is-active");
+    for (const movie of movies) {
+      const { Title, Poster } = movie;
+
+      // Check if Poster is not "N/A"
+      const imgSrc = Poster !== "N/A" ? Poster : "";
+      const option = document.createElement("a");
+      option.classList.add("dropdown-item");
+      option.innerHTML = `
+              <img src="${imgSrc}"  alt="${Title}"/>
+              ${Title}
+              `;
+
+      option.addEventListener("click", () => {
+        input.value = movie.Title;
+        dropdown.classList.remove("is-active");
+        onMovieSelect(movie);
+      });
+      resultsWrapper.appendChild(option);
+    }
+  };
+  input.addEventListener("input", debounce(onInput, 500));
+  document.addEventListener("click", (event) => {
+    if (!root.contains(event.target)) dropdown.classList.remove("is-active");
+  });
+};
--- MovieBox/index.js
+++ MovieBox/index.js
@@ -11,56 +11,9 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 };
 
-const root = document.querySelector(".authcomplete");
-root.innerHTML = `
-<label><b>Search For a Movie</b></label>
-<input class="input" />
-<div class="dropdown">
-  <div class="dropdown-menu">
-	<div class="dropdown-content results"></div>
-  </div>
-</div>
-`;
-
-const input = document.querySelector("input");
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-
-const onInput = async (event) => {
-  const movies = await fetchData(event.target.value);
-
-  if (!movies.length) {
-    dropdown.classList.remove("is-active");
-    return;
-  }
-
-  //  make `resultWrapper` empty if there is a previous fetch.
-  resultsWrapper.innerHTML = "";
-  dropdown.classList.add("is-active");
-  for (const movie of movies) {
-    const { Title, Poster } = movie;
-
-    // Check if Poster is not "N/A"
-    const imgSrc = Poster !== "N/A" ? Poster : "";
-    const option = document.createElement("a");
-    option.classList.add("dropdown-item");
-    option.innerHTML = `
-		  <img src="${imgSrc}"  alt="${Title}"/>
-		  ${Title}
-		  `;
-
-    option.addEventListener("click", () => {
-      input.value = movie.Title;
-      dropdown.classList.remove("is-active");
-      onMovieSelect(movie);
-    });
-    resultsWrapper.appendChild(option);
-  }
-};
-input.addEventListener("input", debounce(onInput, 500));
-document.addEventListener("click", (event) => {
-  if (!root.contains(event.target)) dropdown.classList.remove("is-active");
-});
+createAutoComplete({ root: document.querySelector(".autocomplete") });
+createAutoComplete({ root: document.querySelector(".autocomplete-two") });
+createAutoComplete({ root: document.querySelector(".autocomplete-three") });
 
 onMovieSelect = async (movie) => {
   const response = await axios.get("http://www.omdbapi.com/", {
